0268cce51b48e482a6c4f8c832f31ddd1f6c5972
Alice-Githui <gnyokabi.alice@gmail.com>
2021-03-21 00:43:17 +0300

    Add the event listeners for our forms

--- JS/script.js
+++ JS/script.js
@@ -9,6 +9,9 @@ const deliveryCosts = document.getElementById('deliveryprices')
 
 
 //functions
+function updateTotalCost(){
+
+}
 
 
 
@@ -17,4 +20,60 @@ document.getElementById('checkoutbutton').addEventListener('click', function(eve
     event.preventDefault()
 
     event.target.textContent = `Your order has been received and is being processed. We will be in touch shortly.`
+})
+
+//on type of pizza
+
+pizzaType.addEventListener('change', function(event){
+    event.preventDefault()
+
+    pizzaPrice = event.target.value
+   // console.log(pizzaPrice)
+
+   updateTotalCost()
+})
+
+pizzaSize.addEventListener('change', function(event){
+    event.preventDefault()
+
+    pizzaSizePrice = parseInt(event.target.value)
+    console.log(pizzaSizePrice)
+
+    updateTotalCost()
+})
+
+crustType.addEventListener('change', function(event){
+    event.preventDefault()
+
+    crustTypePrice = parseInt(event.target.value)
+    console.log(crustTypePrice)
+
+    updateTotalCost()
+})
+
+pizzaToppings.addEventListener('change', function(event){
+    event.preventDefault()
+
+    pizzaToppingsPrice = parseInt(event.target.value)
+    console.log(pizzaToppingsPrice)
+
+    updateTotalCost()
+})
+
+pizzaNumbers.addEventListener('change', function(event){
+    event.preventDefault()
+
+    pizzaPriceOnQuantity = parseInt(event.target.value)
+    console.log(pizzaPriceOnQuantity)
+
+    updateTotalCost()
+})
+
+deliveryCosts.addEventListener('change', function(event){
+    event.preventDefault()
+
+    deliveryCostsPaid = parseInt(event.target.value)
+    console.log(deliveryCostsPaid)
+
+    updateTotalCost()
 })
